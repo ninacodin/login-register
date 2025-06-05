5776482998c1cfdc395c2efd8ef82cf901c197ac
ninacodin <supawankongsapcharoen@gmail.com>
2025-06-06 00:01:36 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const expressSession = require('express-session')
+const flash = require('connect-flash')
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(flash())
+app.set('view engine', 'ejs')
+
+app.listen(3000, () => {
+    console.log("App listening on port http://localhost:3000")
+})
