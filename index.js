752a195ae2cfe199ef96beba8e432f97918230d3
ninacodin <supawankongsapcharoen@gmail.com>
2025-06-12 16:04:00 +0700

    create session

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ mongoose.connect('mongodb+srv://admin:1234@cluster0.warx1qk.mongodb.net/?retryWr
 const indexController = require('./controllers/indexController')
 const loginController = require('./controllers/loginController')
 const registerController = require('./controllers/registerController')
+const storeUserController = require('./controllers/storeUserController')
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(flash())
+app.use(expressSession({
+    secret: "node secret"
+}))
 app.set('view engine', 'ejs')
 
 app.get('/', indexController)
 app.get('/login', loginController)
 app.get('/register', registerController)
+app.post('/user/register', storeUserController)
 
 app.listen(3000, () => {
     console.log("App listening on port 3000")
