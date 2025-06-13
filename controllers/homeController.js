12398be54bb898f7be34a37761bf33a74b17cc1e
ninacodin <supawankongsapcharoen@gmail.com>
2025-06-13 22:56:21 +0700

    Create Homepage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,9 @@
-module.exports = (req, res) => {
-    res.render('home')
+const User = require('../models/User')
+
+module.exports = async (req, res) => {
+
+    let UserData = await User.findById(req.session.userId)
+    res.render('home', {
+        UserData
+    })
 }
